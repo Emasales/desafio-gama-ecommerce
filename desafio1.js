2da545887e0d6b36965c017cab07ea139b693403
Emasales <emanuelle.salesh@gmail.com>
2021-07-18 23:19:49 -0300

    first commit

---
+++ desafio1.js
@@ -0,0 +1,12 @@
+function valida (){
+    if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+        alert("Pronto! Você receberá as novidades por emaill.")
+    }else{
+        alert("Por favor, preencha os campos nome e email")
+    }
+}
+
+
+//document.getElementById("botao").addEventListener("click", valida)
+
+//document.getElementById("myForm").submit();
